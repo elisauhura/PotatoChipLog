9f85a9d546927c705f03e99b8d7e4f4478dca25d
Elisa Silva <elisa@silva.moe>
2021-10-06 23:46:49 -0300

    Initial Commit

---
+++ PotatoChipLog/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PotatoChipLog
+//
+//  Created by Elisa Silva on 06/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
